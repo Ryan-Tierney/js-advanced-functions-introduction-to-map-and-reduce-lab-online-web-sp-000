27de9f83052b92cfe499150f683ddb6cbff50f1c
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-09-04 17:39:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 // Your code here
-const array1 = [1, 3, 5, 7, 9];
-
-const mapToNegativize = array1.map(x => x * -1);
-
-console.log(mapToNegativize)
+function mapToNegativize(array) { 
+  array.map(x => x * -1) 
+}
